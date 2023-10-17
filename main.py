0f0177ee669ef464934931ac518265f1244b55d8
Vildan_Idrisov <iv12511.auca.kg>
2023-10-17 16:30:16 +0600

    all operations are working and aouputing results to cmd

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class MatrixCalculator(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("MatrixCalculator.ui", self)
-        self.Operation_for_now = None
+        self.operation_for_now = None
         self.initUI()
 
     def initUI(self):
@@ -18,20 +18,17 @@ class MatrixCalculator(QMainWindow):
         self.Run()
 
     def Run(self):
-        self.bt_add_row1.clicked.connect(self.MatrixAdd)
-        self.bt_add_row2.clicked.connect(self.MatrixAdd)
-        self.bt_add_col1.clicked.connect(self.MatrixAdd)
-        self.bt_add_col2.clicked.connect(self.MatrixAdd)
+        self.bt_add_row1.clicked.connect(self.addToMatrix)
+        self.bt_add_row2.clicked.connect(self.addToMatrix)
+        self.bt_add_col1.clicked.connect(self.addToMatrix)
+        self.bt_add_col2.clicked.connect(self.addToMatrix)
 
-        self.summation.toggled.connect(self.OperationToDo)
-        self.multiplication.toggled.connect(self.OperationToDo)
-        self.subtraction.toggled.connect(self.OperationToDo)
-        self.determinant.toggled.connect(self.OperationToDo)
+        self.summation.toggled.connect(self.operationToDo)
+        self.multiplication.toggled.connect(self.operationToDo)
+        self.subtraction.toggled.connect(self.operationToDo)
+        self.determinant.toggled.connect(self.operationToDo)
 
-    # TODO обвернуть переделку таблицы в матрицу NumPy в функцию
-
-    def TableToMatrix(self, matrix):  # get matrix
-
+    def tableToMatrix(self, matrix):  # get matrix
         list_matrix = []
         list_row_matrix = []
         for i in range(matrix.rowCount()):
@@ -40,15 +37,15 @@ class MatrixCalculator(QMainWindow):
 
             list_matrix.append(list_row_matrix)
             list_row_matrix = []
-        # returns NumPy
-        return np.array(list_matrix)
 
-    def CheckMultiplication(self):
+        return np.array(list_matrix)  # returns NumPy
+
+    def checkMultiplication(self):
         if self.matrix1.columnCount() == self.matrix2.rowCount():
             return True
         return False
 
-    def MatrixAdd(self):
+    def addToMatrix(self):
         sender = self.sender()
         if sender.accessibleName() == "m1 row":
             self.matrix1.setRowCount(self.matrix1.rowCount() + 1)
@@ -62,25 +59,30 @@ class MatrixCalculator(QMainWindow):
         elif sender.accessibleName() == "m2 col":
             self.matrix2.setColumnCount(self.matrix2.columnCount() + 1)
 
-    def OperationToDo(self):
+    def operationToDo(self):
         sender = self.sender()
+        np_matrix1 = self.tableToMatrix(self.matrix1)
+        np_matrix2 = self.tableToMatrix(self.matrix2)
+
         if sender.accessibleName() == "summation":
-            self.Operation_for_now = "summation"
+            print(np_matrix1 + np_matrix2)
+            self.operation_for_now = "summation"
 
-        elif sender.accessibleName() == "multiplication":
-            if self.CheckMultiplication():
-                np_matrix1 = self.TableToMatrix(self.matrix1)
-                np_matrix2 = self.TableToMatrix(self.matrix2)
-
-                print(np_matrix1.dot(np_matrix2))
-
-            self.Operation_for_now = "multiplication"
         # TODO Построить архетиктуру, соединить с логическимим функциями
         elif sender.accessibleName() == "subtraction":
-            self.Operation_for_now = "subtraction"
+            print(np_matrix1 - np_matrix2)
+            self.operation_for_now = "subtraction"
+
+        elif sender.accessibleName() == "multiplication":
+            if self.checkMultiplication():
+                # TODO оформить вывод ответа или ошибок в новое окно по нашатию кнопки
+                print(np_matrix1.dot(np_matrix2))  # cmd output of multiplied matrix
+            self.operation_for_now = "multiplication"
 
         elif sender.accessibleName() == "determinant":
-            self.Operation_for_now = "determinant"
+            # TODO Сделать что бы при выбори детерменанты вторая матрица становилась не изменяемой и исчезала
+            print(np.linalg.det(np_matrix1))
+            self.operation_for_now = "determinant"
 
 
 if __name__ == '__main__':
